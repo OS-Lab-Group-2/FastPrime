a079c9a3bd86a9b6bd06f22e37ed9db690854968
Tithi304 <tithipal306@gmail.com>
2026-02-08 21:55:55 +0530

    Create prime_multiprocess.c

---
+++ prime_multiprocess.c
@@ -0,0 +1,49 @@
+/* * GROUP PROJECT: Prime Benchmark
+ * -----------------------------
+ * Person 1: Main & Input Parsing
+ * Person 2: is_prime() logic
+ * Person 3: get_logical_cores()
+ * Person 4: Time calculation
+ * Person 5: Forking & Process Management
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+
+// ==========================================
+// Roll 11 AREA: Prime Logic
+// ==========================================
+int is_prime(int n) {
+    // TODO: Person 2 implements prime check here
+    return 0; 
+}
+
+// ==========================================
+// Roll 12 AREA: System Info
+// ==========================================
+int get_logical_cores() {
+    // TODO: Person 3 implements core counting here
+    return 1; 
+}
+
+int main(int argc, char *argv[]) {
+// ==========================================
+// Roll 13 AREA: Timer Helper
+// ==========================================
+
+
+// ==========================================
+// Roll 14 AREA: Process Manager
+// ==========================================
+
+
+// ==========================================
+// Roll 15 AREA: Main Function
+// ==========================================
+
+    // TODO: Person 1 connects everything here
+    return 0;
+}
