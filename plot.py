f96049d3a761ed29cd1c053fa9e8095429a2eca2
Tithi304 <tithipal306@gmail.com>
2026-02-08 23:14:36 +0530

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,18 +1,11 @@
 """
 GROUP PROJECT: Python Plotter
 -----------------------------
-Person 1: Main Config & Execution
-Person 2: System Checks
-Person 3: Subprocess Runner
-Person 4: Data Collection Logic
-Person 5: Plotting & Saving
 """
 import subprocess
 import matplotlib.pyplot as plt
 import os
 
-# ==========================================
-# Roll 17 AREA: Runner
 c_executable = "./prime_multiprocess"
 
 ranges = [
@@ -24,7 +17,6 @@ ranges = [
 # Get logical processors dynamically
 max_processes = os.cpu_count()
 plt.figure(figsize=(10, 6))
-# ==========================================
 
 if not os.path.exists(c_executable):
     print("Executable not found. Compile C code first.")
@@ -34,11 +26,6 @@ for rl, rh in ranges:
     print(f"\nRunning benchmarks for range {rl}-{rh}")
     x = []
     y = []
-
-
-# ==========================================
-# Roll 19 AREA: Visualizer
-# ==========================================
 
     for p in range(1, max_processes + 1):
         result = subprocess.run(
@@ -53,12 +40,6 @@ for rl, rh in ranges:
         print(f"Processes: {p} | Time: {time_taken:.6f}s")
 
     plt.plot(x, y, marker='o', label=f"{rl}-{rh}")
-
-
-# ==========================================
-# Roll 20 AREA: Main Configuration
-# ==========================================
-
 
 plt.xlabel("Number of Processes (n)")
 plt.ylabel("Execution Time (seconds)")
