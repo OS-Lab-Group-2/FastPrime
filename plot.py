dc8820a207512e9fbcc4d56db4ce0fc9ebe60486
Tithi304 <tithipal306@gmail.com>
2026-02-08 22:00:54 +0530

    Create plot.py

---
+++ plot.py
@@ -0,0 +1,31 @@
+"""
+GROUP PROJECT: Python Plotter
+-----------------------------
+Person 1: Main Config & Execution
+Person 2: System Checks
+Person 3: Subprocess Runner
+Person 4: Data Collection Logic
+Person 5: Plotting & Saving
+"""
+import subprocess
+import matplotlib.pyplot as plt
+import os
+
+# ==========================================
+# Roll 17 AREA: Runner
+# ==========================================
+
+
+# ==========================================
+# Roll 18 AREA: Data Collector
+# ==========================================
+
+
+# ==========================================
+# Roll 19 AREA: Visualizer
+# ==========================================
+
+
+# ==========================================
+# Roll 20 AREA: Main Configuration
+# ==========================================
